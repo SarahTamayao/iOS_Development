1ece08b0aa2e803e27fcec6f0d20dfc5f498756e
LINNI CAI <cai20170607@gmail.com>
2020-09-27 17:55:46 -0700

    finish collection view

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Linni Cai on 9/27/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
