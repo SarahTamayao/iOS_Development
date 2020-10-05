7a4aa4b34eca77305452e70d8a89f149c6216ef1
LINNI CAI <cai20170607@gmail.com>
2020-10-04 18:48:27 -0700

    add search bar

---
+++ Yelpy/Yelpy/Controllers/SearchBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SearchBarViewController.swift
+//  Yelpy
+//
+//  Created by LINNI on 10/4/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarViewController: UIViewController
+//                               , UITableViewDataSource, UISearchBarDelegate
+{
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
