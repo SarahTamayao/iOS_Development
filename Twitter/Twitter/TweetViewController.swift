766f7f42fce48482b4112bd064013bc893f8a758
LINNI CAI <cai20170607@gmail.com>
2020-10-10 07:17:55 -0700

    create tweet modal view

---
+++ Twitter/Twitter/TweetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by LINNI on 10/10/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
