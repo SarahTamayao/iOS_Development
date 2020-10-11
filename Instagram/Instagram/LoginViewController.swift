434e649634655b906bc885fd38b94e18e0b65bac
LINNI CAI <cai20170607@gmail.com>
2020-10-10 18:54:41 -0700

    add log in and sign up features

--- Instagram/Instagram/LoginViewController.swift
+++ Instagram/Instagram/LoginViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var loginUsername: LoginTextField!
     @IBOutlet weak var loginPassword: LoginTextField!
+    
+    @IBAction func onLogin(_ sender: Any) {
+        let username = loginUsername.text!
+        let password = loginPassword.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) {
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Log in failed: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    @IBAction func onRegister(_ sender: Any) {
+        let user = PFUser()
+        user.username = loginUsername.text
+        user.password = loginPassword.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Sign up failed: \(String(describing: error?.localizedDescription))")
+            }
+            
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +52,7 @@ class LoginViewController: UIViewController {
         loginButton.layer.cornerRadius = 5
         loginUsername.layer.cornerRadius = 5
         loginPassword.layer.cornerRadius = 5
-//        loginUsername.size.height = 100
-//        loginButton.
+
     }
     
 
